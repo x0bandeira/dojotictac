9ebf86d65adcd418be4027f348ba31fa87297f5a
Rafael Bandeira <rafaelbandeira3@gmail.com>
2010-09-02 15:16:59 -0300

    adding tap control: tap to play/pause, double to restart

--- timer.js
+++ timer.js
@@ -4,7 +4,7 @@ function timer() {
       mins, secs, color, text
       duration = 5;
 
-  function tictac() {
+  function update() {
     seccounter++;
     mins = Math.floor(seccounter / 60);
     secs = mins == 0? seccounter: seccounter - (mins * 60);    
@@ -25,19 +25,23 @@ function timer() {
     clearInterval(timer);
   }
 
+  function play() {
+    timer = setInterval(update, 1000);
+  }
+
   var styles = 'body{background:black;color:white;text-align:center;font-weight:bold;font-family:Helvetica,Tahoma,Arial;}div{font-size:350px}'; 
 
   doc.write('<style>' + styles + '</style><button>tic-tac</button><button>pause</button><div id="timer"></div>')
 
   doc.getElementsByTagName('button')[0].onclick = function() {
     stop();
-    timer = setInterval(tictac, 1000);
+    play();
   };
   
   doc.getElementsByTagName('button')[1].onclick = function(e) {
     var text = 'pause';
     if (timer == null) {
-      timer = setInterval(tictac, 1000); 
+      play(); 
     } else { 
       clearInterval(timer); 
       timer = null;
@@ -47,5 +51,29 @@ function timer() {
     e.target.innerHTML = text;
   }
 
-    panel = doc.getElementById('timer');
+  var action, clicks = 0;
+  
+  function tictac() {
+    if (clicks == 2) { 
+      stop(); 
+      play(); 
+    } else {
+      if (timer != null) {
+        clearInterval(timer);
+        timer = null;
+      } else {
+        play();
+      }
+    }
+    clearInterval(action);
+    clicks = 0;
+  }
+  
+  panel = doc.getElementById('timer');
+
+  panel.onclick = function(event) {
+    clicks++;
+    clearInterval(action);
+    action = setInterval(tictac, 500);
+  }
 }
